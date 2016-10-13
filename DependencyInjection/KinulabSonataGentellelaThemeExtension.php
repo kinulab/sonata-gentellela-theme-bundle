23f6b32e14e4bccdd03eed96d709d4d54f14bd54
Johan DESMYTER <johan@etic.yt>
2016-10-13 15:16:56 +0300

    fix checkbox that doesn't show up

--- DependencyInjection/KinulabSonataGentellelaThemeExtension.php
+++ DependencyInjection/KinulabSonataGentellelaThemeExtension.php
@@ -67,6 +67,7 @@ class KinulabSonataGentellelaThemeExtension extends Extension implements Prepend
                     'stylesheets'   => [
                         'bundles/kinulabsonatagentellelatheme/css/bootstrap.min.css',
                         'bundles/kinulabsonatagentellelatheme/css/font-awesome.min.css',
+                        'bundles/sonataadmin/vendor/iCheck/skins/square/blue.css',
                         'bundles/sonatacore/vendor/select2/select2.css',
                         'bundles/sonatacore/vendor/select2-bootstrap-css/select2-bootstrap.min.css',
                         'bundles/kinulabsonatagentellelatheme/css/prettify.min.css',
